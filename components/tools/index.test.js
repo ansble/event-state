976ff6ce535bcdf4920ce87071a0e9f331ba5a3a
Daniel Sellers <daniel@designfrontier.net>
2015-09-25 15:41:39 -0600

    Breaks out listener selection and some other work
    Like whitespace. It cleans up the whitespace in all the files
    and gets everything to spaces.
    
    Listener selection is complex enough that it is now in a
    function in the tools module.

--- components/tools/index.test.js
+++ components/tools/index.test.js
@@ -1,23 +1,46 @@
 'use strict';
 
 const assert = require('chai').assert
-	, tools = require('./index');
+    , tools = require('./index')
+    , Events = require('events')
+    , emitter = new Events();
 
 describe('Tools tests', () => {
 
 
-	it('should return an object of functions', () => {
-		assert.isObject(tools);
-		assert.isFunction(tools.flatten);
-	});
+    it('should return an object of functions', () => {
+        assert.isObject(tools);
+        assert.isFunction(tools.flatten);
+        assert.isFunction(tools.getListener);
+    });
 
-	it('should flatten an array of arrays', () => {
-		const test = tools.flatten(['this', 'is', ['a', 'string', ['in', ['arrays']]]]);
-		
-		test.forEach((item) => {
-			assert.isNotArray(item);
-		});
+    it('should flatten an array of arrays', () => {
+        const test = tools.flatten(['this', 'is', ['a', 'string', ['in', ['arrays']]]]);
+        
+        test.forEach((item) => {
+            assert.isNotArray(item);
+        });
 
-		assert.isArray(test);
-	});
+        assert.isArray(test);
+    });
+
+    describe('getListener Tests', () => {
+        it('should return .on if multiple is passed', () => {
+            const test = tools.getListener(emitter, true);
+
+            assert.strictEqual(test, emitter.on);
+        });
+
+        it('should return .once if available and multiple=false is passed', () => {
+            const test = tools.getListener(emitter, false);
+
+            assert.strictEqual(test, emitter.once);
+        });
+
+        it('should return .once if available and nothing passed for multiple', () => {
+            const test = tools.getListener(emitter);
+            
+            assert.strictEqual(test, emitter.once);
+        });
+    });
 });
