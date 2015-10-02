db7280c7346018a29a98b24925ecc2d9d536d62f
Daniel Sellers <daniel@designfrontier.net>
2015-10-01 23:05:38 -0600

    Bumps the required test coverage to 100%

--- gulpfile.js
+++ gulpfile.js
@@ -49,7 +49,7 @@ gulp.task('test', ['pre-test'], function () {
     // Creating the reports after tests ran
     .pipe(istanbul.writeReports())
     // Enforce a coverage of at least 90%
-    .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }));
+    .pipe(istanbul.enforceThresholds({ thresholds: { global: 100 } }));
 });
 
 // gulp.task('test', function(){
